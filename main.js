61bd1429da11a2264c5932e45cbcbdf2663da546
ryo <ba068082@gmail.com>
2018-02-10 15:54:04 +0900

    表示非表示メソッド追加

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 $(function () {
+    // --------------------------------------------
+    // ナビ切り替え
+    // --------------------------------------------
+    // 要素取得
+    const $changeBtn = $("#nav-change-btn")
+    const $navJa = $("#nav-ja")
+    const $navEn = $("#nav-en")
+
+    // 初期表示
+    $navEn.hide()
+
+    // アクション
+    $changeBtn.click(function () {
+        $navEn.toggle()
+        $navJa.toggle()
+    })
+
+    // --------------------------------------------
+    // 上下スクロール
+    // --------------------------------------------
     // 要素取得
     const $topBtn = $("#scrollTop")
     const $bottomBtn = $("#scrollBottom")
